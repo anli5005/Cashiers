b4d7a0be5ec4070421c07e6a012a1292586d80b4
Anthony <li.anthony917@gmail.com>
2014-04-09 20:35:11 -0400

    Added the main screen

--- Cashiers/Cashiers/main.js
+++ Cashiers/Cashiers/main.js
@@ -2,5 +2,5 @@ $(document).ready(function () {
     // There is JavaScript; show all elements marked 'noJs'
     $(".noJs").attr("class", "");
     
-    
+    $("#content").attr("style", "margin-top: "+($(".toolbar").height() + 16)+"px;");
 });
